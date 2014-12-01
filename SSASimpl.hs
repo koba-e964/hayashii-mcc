c5d00c1037dc2f207770069872478926f8b9dfa1
koba-e964 <silentkiddie-2014@yahoo.co.jp>
2014-12-01 17:40:40 +0900

    [incomplete] SSA Simplification
    Variable renaming and phi elimination are not performed

--- SSASimpl.hs
+++ SSASimpl.hs
@@ -4,6 +4,15 @@ import Id
 import SSA
 import Type
 import Syntax
+import qualified Data.Map as Map
+import Control.Monad.Reader
+
+import Data.Maybe
+
+type Snippet = ([Inst], Term)
+type BlockEnv = Map.Map BlockID Snippet
+
+type M = Reader BlockEnv
 
 simplFundef :: SSAFundef -> SSAFundef
 simplFundef fundef@(SSAFundef {blocks = blks} ) =
@@ -11,8 +20,24 @@ simplFundef fundef@(SSAFundef {blocks = blks} ) =
 
 simplBlocks :: [Block] -> [Block]
 simplBlocks blocks = f where
-  f = blocks
+  ok blk@(Block blkID insts term) =
+    if length insts >= 2 then Nothing else Just (blkID, (insts, term))
+  env = Map.fromList $ catMaybes $ map ok blocks
+  f = map (replace env) blocks
 
+
+
+append :: Snippet -> Block -> Block
+append (inst, term) (Block blkId insts _) = Block blkId (insts ++ inst) term
+
+
+replace :: BlockEnv -> Block -> Block
+replace env blk@(Block blkId insts term) =
+  case term of
+    TJmp dest | blkId /= dest {- to avoid recursion -} && Map.member dest env ->
+      append (env Map.! dest) blk {- TODO variable renaming and phi elimination are not performed. -}
+    _ -> blk
+  
 
 
 {- Simplifies the CFG. -}
