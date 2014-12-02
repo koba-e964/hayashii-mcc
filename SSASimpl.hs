2c2b836b758c37627409d64e36e588fb04a0a705
koba-e964 <silentkiddie-2014@yahoo.co.jp>
2014-12-02 18:29:48 +0900

    Add CFG simplification
    concatenation of block which has at most one instruction to other blocks that jump to it
    test code:
    let rec fibt x a b = if x <= 0 then a else fibt (x - 1) (a + b) a in let rec fib n = fibt n 1 0 in fib 5

--- SSASimpl.hs
+++ SSASimpl.hs
@@ -8,6 +8,9 @@ import qualified Data.Map as Map
 import Control.Monad.Reader
 
 import Data.Maybe
+import qualified Data.List as List
+
+import Debug.Trace
 
 type Snippet = ([Inst], Term)
 type BlockEnv = Map.Map BlockID Snippet
@@ -28,7 +31,51 @@ simplBlocks blocks = f where
 
 
 append :: Snippet -> Block -> Block
-append (inst, term) (Block blkId insts _) = Block blkId (insts ++ inst) term
+append (inst, term) (Block blkId insts _) = Block blkId (insts ++ renamedInst) renamedTerm where
+  f (Inst d op, i) = case d of
+    Just (VId dd) -> [(VId dd, VId (dd ++ "." ++ blkId ++ "." ++ show i))]
+    Nothing -> []
+  novum = Map.fromList $ concatMap f (zip inst [0..])
+  renamedInst = map (replaceInst novum . removePhi blkId) inst
+  renamedTerm = replaceTerm novum term
+
+removePhi blkId (Inst d op) = Inst d $ case op of
+  SPhi ls -> SId $ List.head $ map snd $ filter (\(x,_) -> x == blkId) $ ls
+  _ -> op
+
+replaceVId :: Map.Map VId VId -> VId -> VId
+replaceVId env x =
+  if Map.member x env then
+    env Map.! x
+  else x
+
+replaceOperand :: Map.Map VId VId -> Operand -> Operand
+replaceOperand env x@(OpConst _) = x
+replaceOperand env (OpVar (v :-: t)) = OpVar (replaceVId env v :-: t)
+
+replaceInst :: Map.Map VId VId -> Inst -> Inst
+replaceInst env (Inst a op1) = Inst b op2 where
+  b = case a of
+    Nothing -> Nothing
+    Just x -> Just $ replaceVId env x
+  op2 = replaceOp env op1
+
+replaceOp :: Map.Map VId VId -> Op -> Op
+replaceOp env op = case op of
+  SId x -> SId $ replaceOperand env x
+  SArithBin o x y -> SArithBin o (replaceOperand env x) (replaceOperand env y)
+  SFloatBin o x y -> SFloatBin o (replaceOperand env x) (replaceOperand env y)
+  SCmpBin o x y -> SCmpBin o (replaceOperand env x) (replaceOperand env y)
+  SNeg x -> SNeg (replaceOperand env x)
+  SFNeg x -> SFNeg (replaceOperand env x)
+  SCall lid args -> SCall lid (map (replaceOperand env) args)
+  SPhi ls -> SPhi (map (\(blkID, x) -> (blkID, replaceOperand env x)) ls) 
+
+replaceTerm :: Map.Map VId VId -> Term -> Term
+replaceTerm env term = case term of
+  TRet x -> TRet $ replaceOperand env x
+  TBr x b1 b2 -> TBr (replaceOperand env x) b1 b2
+  TJmp b -> TJmp b
 
 
 replace :: BlockEnv -> Block -> Block
