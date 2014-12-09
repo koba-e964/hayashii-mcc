c0c9efd97ca842cdba32cda09d8b429a38033383
koba-e964 <silentkiddie-2014@yahoo.co.jp>
2014-12-09 21:20:57 +0900

    Remove all warnings in Closure.hs

--- Closure.hs
+++ Closure.hs
@@ -79,7 +79,7 @@ idToLId :: Id -> LId
 idToLId (Id x) = LId x
 
 transSub :: Map VId Type -> Set Id -> KNormal -> State [CFundef] ClosExp
-transSub env known (e :-: t) = fmap (:-: t) $ case e of
+transSub env known (e :-: exprt) = fmap (:-: exprt) $ case e of
   KUnit -> return CUnit
   (KInt i) -> return $ CInt i
   (KFloat f) -> return $ CFloat f
@@ -88,40 +88,40 @@ transSub env known (e :-: t) = fmap (:-: t) $ case e of
   (KFNeg (Id x)) -> return $ CFNeg (VId x)
   (KFloatBin op (Id x) (Id y)) -> return $ CFloatBin op (VId x) (VId y)
   KIf cmp (Id x) (Id y) e1 e2 -> CIf cmp (VId x) (VId y) <$> transSub env known e1 <*> transSub env known e2
-  KLet (Id x) t e1 e2 -> CLet (VId x) t <$> transSub env known e1 <*> transSub (Map.insert (VId x) t env) known e2
+  KLet (Id x) ty e1 e2 -> CLet (VId x) ty <$> transSub env known e1 <*> transSub (Map.insert (VId x) ty env) known e2
   KVar (Id x) -> return $ CVar (VId x)
-  KLetRec (KFundef { KNormal.name = (x@(Id n), t), KNormal.args = yts, KNormal.body = e1 }) e2 -> -- 関数定義の場合
+  KLetRec KFundef{ KNormal.name = (x@(Id n), ty), KNormal.args = yts, KNormal.body = e1 } e2 -> -- 関数定義の場合
       {- 関数定義let rec x y1 ... yn = e1 in e2の場合は、
 	 xに自由変数がない(closureを介さずdirectに呼び出せる)
 	 と仮定し、knownに追加してe1をクロージャ変換してみる -}
     do
       toplevel_backup <- get
-      let env' = Map.insert (VId n) t env
+      let env' = Map.insert (VId n) ty env
       let known' = Set.insert x known
-      let newenv = (Map.fromList (map (\(Id y, z) -> (VId y, z)) yts) `Map.union` env')
+      let newenv = Map.fromList (map (\(Id y, z) -> (VId y, z)) yts) `Map.union` env'
       e1' <- transSub newenv known' e1 
       {- 本当に自由変数がなかったか、変換結果e1'を確認する
        注意: e1'にx自身が変数として出現する場合はclosureが必要!
          (thanks to nuevo-namasute and azounoman; test/cls-bug2.ml参照) -}
       let zs = freeVars e1' `difference` Set.fromList (map (idToVId . fst) yts)
-      (known', e1') <-
+      (known'2, e1'2) <-
         if Set.null zs then return (known', e1') else do
         {- 駄目だったら状態(toplevelの値)を戻して、クロージャ変換をやり直す -}
-        trace ("free variable(s) " ++ show zs ++ " found in function " ++ show x ++ "@.") $
+        trace ("free variable(s) " ++ show zs ++ " found in function " ++ show x ++ "@.") $!
          trace ("function " ++ show x ++ " cannot be directly applied in fact@.") $ return ()
         put toplevel_backup
-        e1' <- transSub newenv known e1
-        return (known, e1')
-      let zs = Set.toList (freeVars e1' `difference` (Set.map idToVId (Set.insert x (Set.fromList (map fst yts)))))  -- 自由変数のリスト
-      let zts = map (\z -> (z, fromJust (Map.lookup z env'))) zs -- ここで自由変数zの型を引くために引数envが必要
-      modify (CFundef { name = (idToVId x, t), args = map (\(Id y, z) -> (VId y, z)) yts, formalFV = zts, body = e1' } :) -- トップレベル関数を追加
-      e2'@(e2'cont :-: _) <- transSub env' known' e2
-      if Set.member (idToVId x) (freeVars e2') then -- xが変数としてe2'に出現するか
-        return $ CMakeCls (idToVId x) t (Closure { entry = idToLId x, actualFV = zs }) e2' -- 出現していたら削除しない
+        e1'' <- transSub newenv known e1
+        return (known, e1'')
+      let zs' = Set.toList (freeVars e1' `difference` Set.map idToVId (Set.insert x (Set.fromList (map fst yts))))  -- 自由変数のリスト
+      let zts = map (\z -> (z, fromJust (Map.lookup z env'))) zs' -- ここで自由変数zの型を引くために引数envが必要
+      modify (CFundef{ name = (idToVId x, ty), args = map (\(Id y, z) -> (VId y, z)) yts, formalFV = zts, body = e1'2 } :) -- トップレベル関数を追加
+      e2'@(e2'cont :-: _) <- transSub env' known'2 e2
+      return $! if Set.member (idToVId x) (freeVars e2') then -- xが変数としてe2'に出現するか
+        CMakeCls (idToVId x) ty Closure{ entry = idToLId x, actualFV = zs' } e2' -- 出現していたら削除しない
       else
-	return (trace ("eliminating closure(s) " ++ show x ++ "@.") e2'cont) -- 出現しなければMakeClsを削除
+	trace ("eliminating closure(s) " ++ show x ++ "@.") e2'cont -- 出現しなければMakeClsを削除
   KApp (Id x) ys | Set.member (Id x) known -> -- 関数適用の場合
-      return $ trace ("directly applying " ++ x ++ "@.") (CAppDir (LId x) (map idToVId ys))
+      return $! trace ("directly applying " ++ x ++ "@.") (CAppDir (LId x) (map idToVId ys))
   KApp (Id f) xs -> return $ CAppCls (VId f) (map idToVId xs)
   KTuple xs -> return $ CTuple (map idToVId xs)
   KLetTuple xts (Id y) e1 -> let transXts = map (\(Id x, t) -> (VId x, t)) xts in
