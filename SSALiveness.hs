65fe3e5441b0f12f8331967a662b9c2c725f70fa
koba-e964 <silentkiddie-2014@yahoo.co.jp>
2015-02-13 17:01:00 +0900

    [incomplete] Fix liveness analysis

--- SSALiveness.hs
+++ SSALiveness.hs
@@ -1,6 +1,8 @@
 module SSALiveness where
 
 import Data.Array.IArray
+import qualified Data.List as List
+import qualified Data.Map as Map
 import Data.Set (Set, union, unions)
 import qualified Data.Set as Set
 
@@ -36,28 +38,45 @@ killInst :: Inst -> Set VId
 killInst (Inst (Just v) _) = Set.singleton v
 killInst (Inst Nothing _) = Set.empty
 
-nextSets :: SSAFundef -> [(BlockID, ([Set VId], [Set VId]))] -> [(BlockID, ([Set VId], [Set VId]))]
-nextSets (SSAFundef _ args_ _ blks) ls =
-  map (g ls) blks where
-  g ls (Block blk insts term) =
-    let Just (setsIn, setsOut) = lookup blk ls in
+newtype LiveInfo = LiveInfo (Map.Map BlockID BlockLive) deriving (Eq)
+data BlockLive = BlockLive ![InstLive] !TermLive deriving (Eq)
+data InstLive = InstLive { liveIn :: !(Set VId), liveOut :: !(Set VId) } deriving (Eq)
+type TermLive = InstLive
+
+instance Show InstLive where
+  show (InstLive i o) = "in=" ++ f i ++ ", out=" ++ f o where
+    f liveset = show (Set.toList liveset)
+instance Show BlockLive where
+  show (BlockLive instl terml) = concatMap (\i -> show i ++ "\n") (instl ++ [terml])
+instance Show LiveInfo where
+  show (LiveInfo info) = List.intercalate "\n" (map show $ Map.toList info)
+
+
+-- nextSets :: SSAFundef -> [(BlockID, ([Set VId], [Set VId]))] -> [(BlockID, ([Set VId], [Set VId]))]
+nextSets :: SSAFundef -> LiveInfo -> LiveInfo
+nextSets (SSAFundef _ _ _ blks) (LiveInfo info) =
+  LiveInfo $ Map.fromList $ map (g info) blks where
+  g info (Block blk insts term) =
+    let (BlockLive instl terml) = info Map.! blk in
     let len = length insts in
-    let newIn = [(setsIn !! i) `union` genInst (insts !! i) | i <- [0 .. len - 1]] ++ [(setsIn !! len) `union` genTerm term] in
-    let newOut = [setsIn !! (i + 1) | i <- [0 .. len - 1]] in
-    (blk, (newIn, newOut))
+    let newIn i = (liveOut (instl !! i) `Set.difference` killInst (insts !! i)) `union` genInst (insts !! i) in
+    let newOut i = liveIn ((instl ++ [terml]) !! (i + 1)) in
+    let termIn = liveOut terml `union` genTerm term in
+    let termOut = Set.empty {- not correct -} in
+    (blk, BlockLive [InstLive (newIn i) (newOut i) | i <- [0 .. len - 1]] (InstLive termIn termOut))
 
 minFix :: Eq q => (q -> q) -> q -> q
 minFix f init = let e = f init in
   if e == init then init else minFix f e
 
-analyzeLiveness :: SSAFundef -> [(BlockID, ([Set VId], [Set VId]))]
+analyzeLiveness :: SSAFundef -> LiveInfo
 analyzeLiveness fundef@(SSAFundef _ args_ _ blks) = minFix (nextSets fundef) w where
-  w = map g blks
+  w = LiveInfo $ Map.fromList $ map g blks where
   g (Block blk insts term) =
     let len = length insts in
-    let newIn = replicate (len + 1) Set.empty in
-    let newOut = replicate len Set.empty in
-    (blk, (newIn, newOut))
+    let emp = InstLive Set.empty Set.empty in
+    let instl = replicate len emp in
+    (blk, BlockLive instl emp)
 
 
 
