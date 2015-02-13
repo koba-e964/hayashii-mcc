c7fb10648e3f14c36eb7161355989c11f5ef7f0a
koba-e964 <silentkiddie-2014@yahoo.co.jp>
2015-02-13 21:37:00 +0900

    Remove all warnings in SSAReduce.hs

--- SSAReduce.hs
+++ SSAReduce.hs
@@ -1,8 +1,6 @@
 module SSAReduce where
 
-import Id
 import SSA
-import Type
 import Syntax
 
 reduceFundef :: SSAFundef -> SSAFundef
@@ -16,7 +14,7 @@ reduceBlock blkIDs (Block blkId insts term) = Block blkId (map g insts) (h term)
     g (Inst dest op) = Inst dest $ case op of
       SArithBin Add x (OpConst (IntConst 0)) -> SId x
       SArithBin Sub x (OpConst (IntConst 0)) -> SId x
-      SArithBin Mul x (OpConst (IntConst 0)) -> SId (OpConst (IntConst 0))
+      SArithBin Mul _ (OpConst (IntConst 0)) -> SId (OpConst (IntConst 0))
       SArithBin Mul x (OpConst (IntConst 1)) -> SId x
       SArithBin Mul x (OpConst (IntConst 2)) -> SArithBin Add x x
       SArithBin Add (OpConst t) (OpVar x) -> SArithBin Add (OpVar x) (OpConst t)
