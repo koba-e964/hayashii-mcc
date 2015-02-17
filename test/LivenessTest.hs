12ac4051d92a0cd810854e5b5881d7e6e27da3f1
koba-e964 <silentkiddie-2014@yahoo.co.jp>
2015-02-17 19:28:08 +0900

    Rewrite test/LivenessTest.hs

--- test/LivenessTest.hs
+++ test/LivenessTest.hs
@@ -21,16 +21,36 @@ testEq msg actual expected = TFH.testCase msg (TH.assertEqual msg expected actua
 tests :: [TF.Test]
 tests = [testEasy]
 
+(<==) :: VId -> Op -> Inst
+a <== b = Inst (Just a) b
+infix 1 <==
+
 testEasy :: TF.Test
-testEasy = TF.testGroup "liveness_easy"
-  [ testEq "easy_ssa" (analyzeLiveness fundef) dat
-  ] where
+testEasy = testEq "easy_ssa" (analyzeLiveness fundef) dat
+  where
   fundef = SSAFundef (LId "f" :-: TInt) [] [] [blk]
   blk = Block "entry"
-    [ Inst (Just "a") (SId (ci32 (0 :: Int)))
-    , Inst (Just "b") (SArithBin Add (OpVar ("a" :-: TInt)) (ci32 (1 :: Int)))
-    , Inst (Just "c") (SArithBin Add (OpVar ("a" :-: TInt)) (ci32 (2 :: Int)))
-    , Inst (Just "d") (SArithBin Add (OpVar ("b" :-: TInt)) (OpVar ("c" :-: TInt)))
+    [ "a" <== SId (ci32 (0 :: Int))
+    , "b" <== SArithBin Add (OpVar ("a" :-: TInt)) (ci32 (1 :: Int))
+    , "c" <== SArithBin Add (OpVar ("a" :-: TInt)) (ci32 (2 :: Int))
+    , "d" <== SArithBin Add (OpVar ("b" :-: TInt)) (OpVar ("c" :-: TInt))
+    ] (TRet (OpVar ("d" :-: TInt)))
+  dat = LiveInfo (Map.singleton "entry" blive)
+  blive = BlockLive
+    [ InstLive empty (singleton "a")
+    , InstLive (singleton "a") (fromList ["a", "b"])
+    , InstLive (fromList ["a", "b"]) (fromList ["b", "c"])
+    , InstLive (fromList ["b", "c"]) (singleton "d")
+    ] (InstLive (singleton "d") empty)
+
+testLoop :: TF.Test
+testLoop = testEq "liveness_loop" (analyzeLiveness fundef) dat where
+  fundef = SSAFundef (LId "f" :-: TInt) [] [] [blk]
+  blk = Block "entry"
+    [ "a" <== SId (ci32 (0 :: Int))
+    , "b" <== SArithBin Add (OpVar ("a" :-: TInt)) (ci32 (1 :: Int))
+    , "c" <== SArithBin Add (OpVar ("a" :-: TInt)) (ci32 (2 :: Int))
+    , "d" <== SArithBin Add (OpVar ("b" :-: TInt)) (OpVar ("c" :-: TInt))
     ] (TRet (OpVar ("d" :-: TInt)))
   dat = LiveInfo (Map.singleton "entry" blive)
   blive = BlockLive
